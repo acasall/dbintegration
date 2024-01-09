596baff64c865b0a872a6129e086d189c6e6e1a2
andyr <andyr@LAPTOP-JKVTPKV5>
2024-01-09 18:33:51 -0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace dbintegration
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudentID = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.lblStudentAge = new System.Windows.Forms.Label();
+            this.txtStudentAge = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStudentID
+            // 
+            this.lblStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentID.Location = new System.Drawing.Point(30, 30);
+            this.lblStudentID.Name = "lblStudentID";
+            this.lblStudentID.Size = new System.Drawing.Size(100, 23);
+            this.lblStudentID.TabIndex = 0;
+            this.lblStudentID.Text = "Student ID:";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentID.Location = new System.Drawing.Point(105, 29);
+            this.txtStudentID.Multiline = true;
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentID.TabIndex = 1;
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentName.Location = new System.Drawing.Point(263, 30);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(100, 23);
+            this.lblStudentName.TabIndex = 2;
+            this.lblStudentName.Text = "Student Name:";
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentName.Location = new System.Drawing.Point(369, 30);
+            this.txtStudentName.Multiline = true;
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentName.TabIndex = 3;
+            // 
+            // lblStudentAge
+            // 
+            this.lblStudentAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentAge.Location = new System.Drawing.Point(516, 30);
+            this.lblStudentAge.Name = "lblStudentAge";
+            this.lblStudentAge.Size = new System.Drawing.Size(100, 23);
+            this.lblStudentAge.TabIndex = 4;
+            this.lblStudentAge.Text = "Student Age:";
+            // 
+            // txtStudentAge
+            // 
+            this.txtStudentAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudentAge.Location = new System.Drawing.Point(603, 30);
+            this.txtStudentAge.Multiline = true;
+            this.txtStudentAge.Name = "txtStudentAge";
+            this.txtStudentAge.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentAge.TabIndex = 5;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(249, 73);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Insert Data";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(428, 73);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(85, 23);
+            this.btnDisplay.TabIndex = 7;
+            this.btnDisplay.Text = "Display Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // grdData
+            // 
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(155, 102);
+            this.grdData.Name = "grdData";
+            this.grdData.Size = new System.Drawing.Size(461, 238);
+            this.grdData.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtStudentAge);
+            this.Controls.Add(this.lblStudentAge);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.lblStudentID);
+            this.Name = "MainForm";
+            this.Text = "DB Integration";
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label lblStudentAge;
+        private System.Windows.Forms.TextBox txtStudentAge;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.DataGridView grdData;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace dbintegration
+{
+    public partial class MainForm : Form
+    {
+        string connstring = "Server =localhost; Database= studentdb;User= root; Password= iHateminions9!";
+        MySqlConnection connection = null; //null indicates no value, global connection with no value
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenDbConnection() //method
+        {
+            try
+            {
+                //creates/establishes a new database connection
+                connection = new MySqlConnection(connstring);
+                //opens the database connection when the connection is established so we can now communiate with it
+                connection.Open();
+            }
+            catch (Exception ex) //exception object of the actual error message
+            {
+                MessageBox.Show("There was an error is establishing the connection: " + ex.Message);
+            }
+        }
+
+        public void CloseDbConnection()
+        {
+            if (connection != null) 
+            {
+                connection.Close();
+            } 
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            OpenDbConnection();
+
+            string studentName= txtStudentName.Text;
+            int studentID, studentAge;
+            int.TryParse(txtStudentID.Text, out studentID);
+            int.TryParse(txtStudentAge.Text, out studentAge);
+
+            string sqlStatement = "Insert into student2 (studentid, studentname, studentage) values("+ studentID +", '"+ studentName +"', "+ studentAge +")";
+            MySqlCommand sqlCommand = new MySqlCommand(sqlStatement, connection); //new statement that executes the command
+            sqlCommand.ExecuteNonQuery(); //executes statement against the studentdb
+            MessageBox.Show("Added data to the Student2 table"); 
+
+            CloseDbConnection();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            OpenDbConnection();
+
+            //creates the sql statement to be executed as a string
+            string sqlStatement = "Select * from student2";
+            //create a MySqlCommand object. Parameters are the sql statement and the db connection object 
+            MySqlCommand sqlCommand = new MySqlCommand(sqlStatement, connection);
+            //Creates a MySqlReader objcted to read data from student
+            MySqlDataReader reader = sqlCommand.ExecuteReader(); //reads all the data from student2
+            //Creates a DataTable object to store data as rows and columns in memory
+            DataTable dt = new DataTable(); //stores the data 
+            dt.Load(reader); 
+            //display data in grdData
+            grdData.DataSource = dt;
+
+            CloseDbConnection();
+        }
+    }
+
+    
+}
